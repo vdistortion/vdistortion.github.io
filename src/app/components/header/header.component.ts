0a2d2f4083de661ba19e8af9adc5edbf46d68846
Валентин <astrotrain55@gmail.com>
2024-07-01 15:00:40 +0500

    header menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { ArtistService } from '../../services/artist.service';
 import artists from '../../../db';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
