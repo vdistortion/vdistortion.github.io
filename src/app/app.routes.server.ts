3fed3d62f279e8ac43a402660063a0eff3820d0f
Валентин <27902270+vdistortion@users.noreply.github.com>
2026-02-26 21:44:15 +0500

    fixed ssr

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,83 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import artists from '../db';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'artist/:artist',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return Object.keys(artists).map((id) => ({ artist: id }));
+    },
+  },
+  {
+    path: 'artist/:artist/video',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return Object.keys(artists).map((id) => ({ artist: id }));
+    },
+  },
+  {
+    path: 'artist/:artist/images',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return Object.keys(artists).map((id) => ({ artist: id }));
+    },
+  },
+  {
+    path: 'artist/:artist/images/:gallery',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const result: { artist: string; gallery: string }[] = [];
+      Object.entries(artists).forEach(([artist, item]) => {
+        item.artist.images?.forEach((_, gallery) => {
+          result.push({ artist, gallery: String(gallery) });
+        });
+      });
+      return result;
+    },
+  },
+  {
+    path: 'artist/:artist/songs',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return Object.keys(artists).map((id) => ({ artist: id }));
+    },
+  },
+  {
+    path: 'artist/:artist/songs/other',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return Object.keys(artists).map((id) => ({ artist: id }));
+    },
+  },
+  {
+    path: 'artist/:artist/song/:song',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const result: { artist: string; song: string }[] = [];
+      Object.entries(artists).forEach(([artist, item]) => {
+        Object.keys(item.songs).forEach((song) => {
+          result.push({ artist, song });
+        });
+      });
+      return result;
+    },
+  },
+  {
+    path: 'artist/:artist/album/:album',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const result: { artist: string; album: string }[] = [];
+      Object.entries(artists).forEach(([artist, item]) => {
+        Object.keys(item.albums).forEach((album) => {
+          result.push({ artist, album });
+        });
+      });
+      return result;
+    },
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Server,
+    renderMode: RenderMode.Prerender,
   },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { ArtistService } from './services/artist.service';
@@ -10,15 +11,19 @@ import { ArtistService } from './services/artist.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
+  private platformId = inject(PLATFORM_ID);
+
   constructor(private artistService: ArtistService) {
     this.artistService.artist$.subscribe(this.faviconChange);
   }
 
   faviconChange(artistId: string) {
-    const link: HTMLLinkElement = document.querySelector('[rel="icon"]')!;
-    const icon: string =
-      artistId.length && artistId === 'master' ? 'artist/master/favicon.ico' : 'favicon.ico';
+    if (isPlatformBrowser(this.platformId)) {
+      const link: HTMLLinkElement = document.querySelector('[rel="icon"]')!;
+      const icon: string =
+        artistId.length && artistId === 'master' ? 'artist/master/favicon.ico' : 'favicon.ico';
 
-    if (link.href !== icon) link.href = icon;
+      if (link.href !== icon) link.href = icon;
+    }
   }
 }
